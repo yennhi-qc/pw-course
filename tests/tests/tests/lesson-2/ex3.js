28cccf82bb3f000c45c5259ae2ceeadcb6b40fd1
Nhi Nguyen <yennhi0321@gmail.com>
2025-11-23 23:46:31 +0700

    add lesson-2

---
+++ tests/tests/tests/lesson-2/ex3.js
@@ -0,0 +1,12 @@
+const a = 5;
+if (a > 0){
+   console.log("Giá trị bạn nhập là số dương");
+}
+
+else if (a < 0){
+   console.log("Giá trị bạn nhập là số âm");
+}
+
+else {
+   console.log("Giá trị bạn nhập là số 0");
+}
---
+++ tests/tests/tests/lesson-2/ex5.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 100; i++ ) {
+   if (i % 2 === 0 ){
+       console.log (`Số ${i} là số chẵn`);
+   } else if (i % 2 === 1){
+       console.log (`Số ${i} là số lẻ`);
+   }
+}
